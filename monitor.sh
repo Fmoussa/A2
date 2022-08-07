ee6f0c586604d155702e3eb5ff4041a8cd98687d
Finn Moussa <67665060+Fmoussa@users.noreply.github.com>
2022-08-07 15:35:29 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -2,15 +2,15 @@
 
 set -x 
 
-echo '' | sudo tee -a webserver_log/temporary.log
+echo '' | sudo tee -a webserver_log/temp.log
 
-diflog=$(sudo diff /webserver_log/unauthorized.log /webserver_log/temporary.log)
+diflog=$(sudo diff /webserver_log/unauth.log /webserver_log/temp.log)
 
 if [ "$diflog" != "" ]
 
 then
   echo -n "Unauthorized access report: $diflog" | mail -s "New Unauthorized Access" bm944281@wcupa.edu
-  sudo cat /webserver_log/unauthorized.log | sudo tee /webserver_log/temporary.log
+  sudo cat /webserver_log/unauth.log | sudo tee /webserver_log/temp.log
         
 else
         echo "No unauthaccess" | mail -s "No unauthorized access." bm944281@wcupa.edu
